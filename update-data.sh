74ab490e67520032cb709e77d789cb93903fe504
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-01-06 23:06:56 -0300

    refactor: change var name time -> date

--- update-data.sh
+++ update-data.sh
@@ -4,8 +4,8 @@
 for f in *.pdf; do 
     pdftotext $f; 
     txt=$( echo "${f%pdf}txt" );
-    time=$( echo $txt | awk 'BEGIN{ FS="_" }{print $6}' | sed 's/\.txt//g' | awk 'BEGIN{ FS="-" }{print $3 "-" $2 "-" $1}');
+    date=$( echo $txt | awk 'BEGIN{ FS="_" }{print $6}' | sed 's/\.txt//g' | awk 'BEGIN{ FS="-" }{print $3 "-" $2 "-" $1}');
     data=$( head -30 $txt | grep -P '^[0-9]+' | sed 's/\.//g' | tr '\n' , );
-    echo $time,$data | sed 's/,$//g' | sed 's/\*//g';
+    echo $date,$data | sed 's/,$//g' | sed 's/\*//g';
     rm $txt;
 done >> covid-bh.csv
