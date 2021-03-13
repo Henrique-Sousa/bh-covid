a490f7b53e35b1c01f9326266e7c24d379ff75a2
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-03-13 10:14:57 -0300

    do not allow digits without a leading zero in date

--- update-data.sh
+++ update-data.sh
@@ -6,7 +6,7 @@ for f in *.pdf; do
     txt=$( echo "${f%pdf}txt" );
     date=$( echo $txt | awk 'BEGIN{ FS="_" }{print $6}' | sed 's/\.txt//g' | awk 'BEGIN{ FS="-" }{print $3 "-" $2 "-" $1}');
     data=$( head -30 $txt | grep -P '^[0-9]+' | sed 's/\.//g' | tr '\n' , );
-    echo $date,$data | sed 's/,$//g' | sed 's/\*//g';
+    echo $date,$data | sed 's/,$//g' | sed 's/\*//g' | sed 's/\-\([0-9]\),/-0\1,/g' | sed 's/\-\([0-9]\)-/-0\1-/g';
     rm $txt;
     rm $f;
 done >> covid-bh.csv
